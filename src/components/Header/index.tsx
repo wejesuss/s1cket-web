c4a253a8bb9bd30f35cfaec0312066302de79b68
Wemerson Jesus <wejesuss@gmail.com>
2020-08-22 19:05:46 -0300

    Chore/burger navigator (#2)
    * Create burger navigator
    
    * Block scroll when burger navigator is actives

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
@@ -15,8 +15,14 @@ interface HeaderProps {
 
 // eslint-disable-next-line react/prop-types
 const Header: React.FC<HeaderProps> = ({ name, hasFavorites, activePage }) => {
+  const [opened, setOpened] = useState(false)
+
+  function handleToggleNavigatorState() {
+    document.body.classList.toggle('blocked', !opened)
+    setOpened(!opened)
+  }
   return (
-    <header id="header" className={name}>
+    <header id="header" className={opened ? name + ' up' : name}>
       <div className="header-box">
         <div className="logo">
           <Link to="/">
@@ -28,7 +34,7 @@ const Header: React.FC<HeaderProps> = ({ name, hasFavorites, activePage }) => {
             <Link to="/favorites?type=stock">Ver Favoritos</Link>
           </div>
         )}
-        <nav className="menu">
+        <nav className={opened ? 'menu opened' : 'menu'}>
           <ul>
             <li>
               <Link to="/" className={activePage === 'Home' ? 'active' : ''}>
@@ -55,6 +61,39 @@ const Header: React.FC<HeaderProps> = ({ name, hasFavorites, activePage }) => {
               </Link>
             </li>
           </ul>
+          <div id="burger" onClick={handleToggleNavigatorState}>
+            <i></i>
+            <i></i>
+            <i></i>
+            <div className="modal">
+              <ul className="menu-burger">
+                <li>
+                  <Link to="/" className={activePage === 'Home' ? 'active' : ''}>
+                    <img src={home} alt="Home" />
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/stocks"
+                    className={activePage === 'Stocks' ? 'active' : ''}
+                  >
+                    <img src={wallet} alt="Wallet" />
+                    Ações
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/cripto"
+                    className={activePage === 'Bitcoin' ? 'active' : ''}
+                  >
+                    <img src={bitcoin} alt="Bitcoin" />
+                    Cripto
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
       </div>
     </header>
