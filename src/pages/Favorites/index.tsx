837f971e1be41c927668b76d9a0d35e9ff5e2ee1
Wemerson Jesus <wejesuss@gmail.com>
2020-09-09 09:29:03 -0300

    Create ExchangeRate favorites

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,40 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useHistory } from 'react-router';
+import { useHistory } from 'react-router';
 
 import Header from '../../components/Header';
 import StockArticle from '../../components/StockArticle';
 import CryptoArticle from '../../components/CryptoArticle';
+import ExchangeArticle from '../../components/ExchangeArticle';
 
-import { PolishedSearch } from '../../services/api-types';
+import { getFavoriteQueryParam, waitTwoMinutes } from '../../Helpers';
+import api from '../../services/api';
+import { PolishedExchangeRate } from '../../services/api-types';
 
 import './styles.css';
 
-interface PolishedCrypto {
+interface Search {
+  id: string;
+  symbol: string;
+  type: string;
+  name: string;
+  currency: string;
+  region: string;
+};
+
+interface Crypto {
+  id: string;
   symbol: string;
   name: string;
   currency: string;
   currencyName: string;
 };
 
+interface ExchangeRate {
+  id: string;
+  fromCurrencyCode: string;
+  fromCurrencyName: string;
+  toCurrencyCode: string;
+  toCurrencyName: string;
+  exchangeRate: string;
+  bidPrice: string;
+  askPrice: string;
+};
+
+type Searches = Search[];
+type Cryptos = Crypto[];
+type ExchangeRates = ExchangeRate[];
+type Favorites = Searches | Cryptos | ExchangeRates;
+
 const Favorites: React.FC = () => {
-  const history = useHistory()
-  const urlparams = useLocation().search;
-  const [params, setParams] = useState(urlparams)
-  const [favorites, setFavorites] = useState<PolishedSearch | PolishedCrypto[]>([]);
+  const history = useHistory();
+  const [params, setParams] = useState(history.location.search);
+  const [favorites, setFavorites] = useState<Favorites>([]);
   const [isResultsEmpty, setIsResultsEmpty] = useState(false);
 
   useEffect(() => {
     const param = getFavoriteQueryParam(params);
 
-    const favorites: PolishedSearch | PolishedCrypto[] = JSON.parse(localStorage.getItem(`favorites-${param}`) || '[]');
+    let favorites: Favorites = JSON.parse(localStorage.getItem(`favorites-${param}`) || '[]');
     if(favorites.length < 1) {
       setIsResultsEmpty(true)
     } else {
-      setFavorites(favorites)
-      setIsResultsEmpty(false)
+      setFavorites(favorites);
+      setIsResultsEmpty(false);
     }
-  // eslint-disable-next-line
+
+    setParams(param);
+    // eslint-disable-next-line
   }, []);
+
+  async function updateExchangeRate(id: string, from_currency: string, to_currency: string) {
+    if(!waitTwoMinutes()) {
+      return;
+    }
+
+    try {
+      const { data } = await api.get<PolishedExchangeRate>(
+        "/currencies/exchange",
+        {
+          params: {
+            from_currency,
+            to_currency
+          },
+        },
+      );
+
+      const newFavorites = (favorites as ExchangeRates).map((oldFavorite) => {
+        if(oldFavorite.id === id) {
+          return {
+            ...oldFavorite,
+            askPrice: data.currencyExchangeRate?.askPrice || "-",
+            bidPrice: data.currencyExchangeRate?.bidPrice || "-",
+            exchangeRate: data.currencyExchangeRate?.exchangeRate || "-"
+          }
+        }
+
+        return oldFavorite;
+      });
+
+      localStorage.setItem('last', `${Date.now()}`);
+      setFavorites(newFavorites);
+      setIsResultsEmpty(false);
+    } catch (error) {
+      console.error(error);
+      alert('Erro inesperado, tente novamente em breve');
+    }
+  }
 
   function seriesAction(series: string, symbol: string, market?: string) {
     history.push(`/${params}`, { symbol, series, market })
@@ -43,7 +111,7 @@ const Favorites: React.FC = () => {
   return (
     <>
       <Header name="favorites" activePage="Favorites" />
-      <div id="favorites" className={params}>
+      <div id="favorites">
         <main>
           {favorites.length > 0 && (
             <>
@@ -51,34 +119,48 @@ const Favorites: React.FC = () => {
                 Estes são os seus Favoritos
               </h1>
               <div className="results">
-                {params === "stocks" ? (
-                  (favorites as PolishedSearch).map((result) => (
-                    <StockArticle
-                      key={result.symbol}
-                      type={result.type}
-                      name={result.name}
-                      currency={result.currency}
-                      region={result.region}
-                      symbol={result.symbol}
-                      seriesActionIntraday={() => seriesAction("intraday", result.symbol)}
-                      seriesActionDaily={() => seriesAction("daily", result.symbol)}
-                      seriesActionWeekly={() => seriesAction("weekly", result.symbol)}
-                    />
-                  ))
-                ) : (
-                  (favorites as PolishedCrypto[]).map(result => (
-                    <CryptoArticle
-                      key={result.symbol + result.currency}
-                      symbol={result.symbol}
-                      name={result.name}
-                      currency={result.currency}
-                      currencyName={result.currencyName}
-                      seriesActionDaily={() => seriesAction("daily", result.symbol, result.currency)}
-                      seriesActionWeekly={() => seriesAction("weekly", result.symbol, result.currency)}
-                      seriesActionMonthly={() => seriesAction("monthly", result.symbol, result.currency)}
-                    />
-                  ))
-                )}
+                {params === "stocks" && (favorites as Searches).map((result) => (
+                  <StockArticle
+                    key={result.id}
+                    type={result.type}
+                    name={result.name}
+                    currency={result.currency}
+                    region={result.region}
+                    symbol={result.symbol}
+                    seriesActionIntraday={() => seriesAction("intraday", result.symbol)}
+                    seriesActionDaily={() => seriesAction("daily", result.symbol)}
+                    seriesActionWeekly={() => seriesAction("weekly", result.symbol)}
+                  />
+                ))}
+
+                {params === "crypto" && (favorites as Cryptos).map(result => (
+                  <CryptoArticle
+                    key={result.id}
+                    symbol={result.symbol}
+                    name={result.name}
+                    currency={result.currency}
+                    currencyName={result.currencyName}
+                    seriesActionDaily={() => seriesAction("daily", result.symbol, result.currency)}
+                    seriesActionWeekly={() => seriesAction("weekly", result.symbol, result.currency)}
+                    seriesActionMonthly={() => seriesAction("monthly", result.symbol, result.currency)}
+                  />
+                ))}
+
+                {params === "exchange" && (favorites as ExchangeRates).map(result => (
+                  <ExchangeArticle
+                    key={result.id}
+                    fromCurrencyCode={result.fromCurrencyCode}
+                    fromCurrencyName={result.fromCurrencyName}
+                    toCurrencyCode={result.toCurrencyCode}
+                    toCurrencyName={result.toCurrencyName}
+                    exchangeRate={result.exchangeRate}
+                    bidPrice={result.bidPrice}
+                    askPrice={result.askPrice}
+                    updateExchangeRate={
+                      () => updateExchangeRate(result.id, result.fromCurrencyCode, result.toCurrencyCode)
+                    }
+                  />
+                ))}
               </div>
               <p className="results-end">Parece que chegamos ao fim!</p>
             </>
