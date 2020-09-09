2b1558e8225e6e85eaf9fd5826a4bd7cb86b2cec
Wemerson Jesus <wejesuss@gmail.com>
2020-09-09 09:02:01 -0300

    Refactor: Dynamically add/remove favorites

--- src/components/Heart/index.tsx
+++ src/components/Heart/index.tsx
@@ -4,97 +4,110 @@ import heartIcon from '../../assets/heart.svg';
 import heartFullIcon from '../../assets/heart-full.svg';
 import './styles.css';
 
+interface StocksProps {
+  id: string;
+  currency: string;
+  name: string;
+  region: string;
+  symbol: string;
+  type: string;
+};
+
+interface CryptoProps {
+  id: string;
+  symbol: string;
+  name: string;
+  currency: string;
+  currencyName: string;
+};
+
+interface ExchangeProps {
+  id: string;
+  fromCurrencyCode: string;
+  fromCurrencyName: string;
+  toCurrencyCode: string;
+  toCurrencyName: string;
+  exchangeRate: string;
+  bidPrice: string;
+  askPrice: string;
+};
+
 interface HeartProps {
-  action: 'stocks' | 'crypto';
-  symbol: string;
-  name: string;
-  currency: string;
-  currencyName?: string;
-  region?: string;
-  type?: string;
+  action: "stocks" | "crypto" | "exchange";
+  stocksData?: StocksProps;
+  cryptoData?: CryptoProps;
+  exchangeData?: ExchangeProps;
 }
 
-export interface FavoritesData {
-  name: string;
-  currency: string;
-  currencyName?: string;
-  region?: string;
-  type?: string;
-}
+export type FavoritesData = StocksProps |CryptoProps | ExchangeProps;
 
-export interface Favorites extends FavoritesData {
-  symbol: string;
-}
+type Mapped<T> = {
+  [P in keyof T]: T[P]
+}[]
 
-const Heart: React.FC<HeartProps> = ({ action, symbol, currency, currencyName, name, region = "United States", type = "Equity" }) => {
+export type Favorites = Mapped<FavoritesData>
+
+const Heart: React.FC<HeartProps> = (
+  { action,
+    stocksData,
+    cryptoData,
+    exchangeData
+  }) => {
+
   const [isFavorited, setIsFavorited] = useState(false);
+  const [favoriteData] = useState({
+    ...stocksData,
+    ...cryptoData,
+    ...exchangeData
+  });
+
+  function toggleAddRemoveFavorites(data: FavoritesData, oldFavorites: Favorites) {
+    const index = oldFavorites.findIndex(favorite => favorite.id === data.id);
+    let wasRemoved = false;
+
+    if(index !== -1) {
+      oldFavorites.splice(index, 1);
+      wasRemoved = true;
+    } else {
+      oldFavorites.push({...data});
+    }
+
+    setIsFavorited(!wasRemoved);
+    return oldFavorites;
+  };
 
   function handleSetFavorites(
-    symbol: string,
     data: FavoritesData
-  ): boolean {
-    let wasRemoved = false;
-    let favorites = localStorage.getItem(`favorites-${action}`)
+  ) {
+    let favorites = localStorage.getItem(`favorites-${action}`);
+
     if(favorites) {
-      let oldFavorites: Favorites[] = JSON.parse(favorites);
-      const index = oldFavorites.findIndex(favorited => {
-        if(currencyName && favorited.currencyName) {
-          return (favorited.symbol + favorited.currencyName) === (symbol + currencyName);
-        }
+      let oldFavorites: Favorites = JSON.parse(favorites);
 
-        return favorited.symbol === symbol;
-      });
-
-      if(index !== -1) {
-        oldFavorites.splice(index, 1);
-        wasRemoved = true
-      } else {
-        oldFavorites.push({...data, symbol});
-      }
+      oldFavorites = toggleAddRemoveFavorites(data, oldFavorites);
 
       localStorage.setItem(`favorites-${action}`, JSON.stringify(oldFavorites));
     } else {
-      localStorage.setItem(`favorites-${action}`, JSON.stringify([{...data, symbol}]))
+      localStorage.setItem(`favorites-${action}`, JSON.stringify([{...data}]));
+      setIsFavorited(true);
     }
-
-    return wasRemoved;
   }
 
   function handleToggleFavorites() {
-    let favoriteData: FavoritesData = {
-      currency,
-      name,
-      currencyName
-    }
-
-    if(action === "stocks") {
-      favoriteData = {
-        ...favoriteData,
-        region,
-        type
-      }
-    }
-
-    const wasRemoved = handleSetFavorites(symbol, favoriteData);
-    setIsFavorited(!wasRemoved)
+    handleSetFavorites(favoriteData as FavoritesData);
   }
 
   useEffect(() => {
-    const favorites: Favorites[] = JSON.parse(localStorage.getItem(`favorites-${action}`) || '[]')
-    const index = favorites.findIndex(favorited => {
-      if(currencyName && favorited.currencyName) {
-        return (favorited.symbol + favorited.currency) === (symbol + currency);
-      }
+    const favorites: Favorites = JSON.parse(localStorage.getItem(`favorites-${action}`) || '[]');
 
-      return favorited.symbol === symbol;
-    })
+    const index = favorites.findIndex(favorite => favorite.id === (favoriteData as FavoritesData).id);
+
+    setIsFavorited(false);
 
     if(index !== -1) {
-      setIsFavorited(true)
-    } else {
-      setIsFavorited(false)
+      setIsFavorited(true);
     }
-  }, [symbol, action, currencyName, currency])
+  }, [action, favoriteData]);
 
   return (
     <span
